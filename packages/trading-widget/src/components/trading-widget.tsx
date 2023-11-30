c67adc610c930cc9ce0721ecfa932ff8a498cb33
Artsiom Yavorski <artsiom.yavorski@gmail.com>
2023-11-30 21:39:25 +0200

    feat: setup storybook

--- packages/trading-widget/src/components/trading-widget.tsx
+++ packages/trading-widget/src/components/trading-widget.tsx
@@ -1,5 +1,10 @@
+import '../styles/index.css'
 import type { FC } from 'react'
 
 export const TradingWidget: FC = () => {
-  return <div className="tw-bg-red">Test</div>
+  return (
+    <div className="dtw-text-xl dtw-font-medium dtw-text-amber-300">
+      Trading Widget
+    </div>
+  )
 }
