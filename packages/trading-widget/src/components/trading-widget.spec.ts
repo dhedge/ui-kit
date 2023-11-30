63340da5c9ea5b984d52a5833c36063ddfdb35ae
Dmitry Lobachevsky <dmitriylobachevskiy@gmail.com>
2023-11-30 18:42:48 +0300

    feat: core trading widget setup

---
+++ packages/trading-widget/src/components/trading-widget.spec.ts
@@ -0,0 +1,7 @@
+import { TradingWidget } from './trading-widget'
+
+describe('tradingWidget', () => {
+  it('should work', () => {
+    expect(TradingWidget).toBeDefined()
+  })
+})
---
+++ packages/trading-widget/src/components/trading-widget.tsx
@@ -0,0 +1,5 @@
+import type { FC } from 'react'
+
+export const TradingWidget: FC = () => {
+  return <div className="tw-bg-red">Test</div>
+}
