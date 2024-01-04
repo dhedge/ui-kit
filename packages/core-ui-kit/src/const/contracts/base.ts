0725e406da4be11189e4e8b17f3b8873077c4eee
Dmitry Lobachevsky <dmitriylobachevskiy@gmail.com>
2024-01-04 15:12:45 +0300

    refactor(Contracts): Remove EasySwapper from Base network

--- packages/core-ui-kit/src/const/contracts/base.ts
+++ packages/core-ui-kit/src/const/contracts/base.ts
@@ -1,6 +1,1 @@
-import { AddressZero } from '../web3'
-
 export const FACTORY_ADDRESS_BASE = '0x49Afe3abCf66CF09Fab86cb1139D8811C8afe56F'
-
-// TODO: update
-export const EASY_SWAPPER_ADDRESS_BASE = AddressZero
