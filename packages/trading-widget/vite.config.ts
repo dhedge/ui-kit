0fc5910677c491fbce0ba97114dc5df1261c14dc
Dmitry Lobachevsky <dmitriylobachevskiy@gmail.com>
2024-03-05 18:13:45 +0300

    build(Trading Widget):  peer deps

--- packages/trading-widget/vite.config.ts
+++ packages/trading-widget/vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 
+import { peerDependencies } from './package.json'
+
 import * as path from 'path'
 
 export default defineConfig({
@@ -28,7 +30,7 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.tsx',
-      name: 'trading-widget',
+      name: '@dhedge/trading-widget',
       fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
@@ -36,7 +38,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: [],
+      external: [...Object.keys(peerDependencies)],
     },
   },
 
