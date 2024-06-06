661e08410bdf39aad3a0111d1b5c5ba5d2364b83
Dmitry Lobachevsky <dmitriylobachevskiy@gmail.com>
2024-06-06 10:50:07 +0200

    style(Trading Widget): update trading-widget background color

--- packages/trading-widget/src/trading-widget/components/trading-widget.stories.tsx
+++ packages/trading-widget/src/trading-widget/components/trading-widget.stories.tsx
@@ -6,13 +6,6 @@ import { TradingWidget as TradingWidgetComponent } from '.'
 
 const meta: Meta<typeof TradingWidgetComponent> = {
   component: TradingWidgetComponent,
-  decorators: [
-    (Story) => (
-      <div className="dtw-bg-themeDark-800 dtw-text-white dtw-rounded-xl">
-        <Story />
-      </div>
-    ),
-  ],
   args: {
     config: {
       params: {
