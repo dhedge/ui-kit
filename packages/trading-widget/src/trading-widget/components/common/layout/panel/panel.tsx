0a8bd0f7e09e75350e64c00ff8c7aaa1feb0eef1
Yavorski <artsiom.yavorski@scorum.net>
2024-08-06 23:05:08 +0300

    feat: migrate to headlessui v2

--- packages/trading-widget/src/trading-widget/components/common/layout/panel/panel.tsx
+++ packages/trading-widget/src/trading-widget/components/common/layout/panel/panel.tsx
@@ -1,7 +1,7 @@
-import { Tab } from '@headlessui/react'
+import { TabPanel } from '@headlessui/react'
 import type { FC, PropsWithChildren } from 'react'
 export const Panel: FC<PropsWithChildren> = ({ children }) => (
-  <Tab.Panel className="dtw-pt-[var(--panel-content-pt)] dtw-pb-[var(--panel-content-pb)] dtw-px-[var(--panel-content-px)] dtw-flex dtw-flex-col dtw-gap-[var(--panel-content-gap,var(--panel-gap))]">
+  <TabPanel className="dtw-pt-[var(--panel-content-pt)] dtw-pb-[var(--panel-content-pb)] dtw-px-[var(--panel-content-px)] dtw-flex dtw-flex-col dtw-gap-[var(--panel-content-gap,var(--panel-gap))]">
     {children}
-  </Tab.Panel>
+  </TabPanel>
 )
