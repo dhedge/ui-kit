32da6e3a12d2db0368801be2e720db39edf5dce1
Dmitry Lobachevsky <dmitriylobachevskiy@gmail.com>
2024-08-27 12:37:50 +0200

    feat: add sARBy and sETHy to synthetix vaults list

--- packages/trading-widget/src/core-kit/const/synthetix.ts
+++ packages/trading-widget/src/core-kit/const/synthetix.ts
@@ -9,9 +9,10 @@ export const DHEDGE_SYNTHETIX_V3_ASSETS_MAP: Record<ChainId, Address> = {
 }
 
 export const DHEDGE_SYNTHETIX_V3_VAULT_ADDRESSES: Address[] = [
-  '0x90fd55a7ef1af647e93ae96a17bcb3b6a2df0e02', // DNY3 Optimism
   '0xc1e02884af4a283ca25ab63c45360d220d69da52', // Synthetix USDC Yield Base
   '0xc3198eb5102fb3335c0e911ef1da4bc07e403dd1', // Synthetix USD Yield Arbitrum
+  '0xddd6b1f34e12c0230ab23cbd4514560b24438514', // Synthetix ARB Yield Arbitrum
+  '0xe9b5260d99d283ff887859c569baf8ad1bd12aac', // Synthetix ETH Yield Arbitrum
 ]
 
 export const SYNTHETIX_V3_POSITION_DEBT_ARGUMENTS: Record<string, string[]> = {
@@ -22,6 +23,16 @@ export const SYNTHETIX_V3_POSITION_DEBT_ARGUMENTS: Record<string, string[]> = {
   ],
   '0xc3198eb5102fb3335c0e911ef1da4bc07e403dd1': [
     '170141183460469231731687303715884106252', // Synthetix USD Yield Arbitrum ID
+    '1',
+    '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+  ],
+  '0xddd6b1f34e12c0230ab23cbd4514560b24438514': [
+    '170141183460469231731687303715884106481', //  Synthetix ARB Yield Arbitrum ID
+    '1',
+    '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+  ],
+  '0xe9b5260d99d283ff887859c569baf8ad1bd12aac': [
+    '170141183460469231731687303715884106484', // Synthetix ETH Yield Arbitrum ID
     '1',
     '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
   ],
