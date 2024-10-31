7a209021d9f40a580b88e937c50f2cc9f8479535
Denis Ignatovich <denis.ignatovich.m@gmail.com>
2024-10-31 16:21:07 +0100

    refactor: useIsCustomCooldownDeposit, default to always use custom

--- packages/trading-widget/src/core-kit/hooks/trading/deposit-v2/use-is-custom-cooldown-deposit.test.ts
+++ packages/trading-widget/src/core-kit/hooks/trading/deposit-v2/use-is-custom-cooldown-deposit.test.ts
@@ -1,16 +1,10 @@
-import { act } from '@testing-library/react'
-
 import { useTradingPanelPoolConfig } from 'core-kit/hooks/state'
 import { TEST_ADDRESS } from 'tests/mocks'
 import { renderHook } from 'tests/test-utils'
 
 import { useIsCustomCooldownDeposit } from './use-is-custom-cooldown-deposit'
-import { usePoolDynamicContractData } from '../../pool'
 import { usePoolStatic } from '../../pool/multicall'
 
-vi.mock('core-kit/hooks/pool', () => ({
-  usePoolDynamicContractData: vi.fn(),
-}))
 vi.mock('core-kit/hooks/pool/multicall', () => ({
   usePoolStatic: vi.fn(),
 }))
@@ -32,50 +26,20 @@ describe('useIsCustomCooldownDeposit', () => {
     vi.restoreAllMocks()
   })
 
-  it('should return false when entry fee is 0 or custom cooldown deposit is not allowed', () => {
+  it('should return false when custom cooldown deposit is not allowed', () => {
     vi.mocked(usePoolStatic).mockReturnValueOnce({
       data: { isCustomCooldownDepositAllowed: false },
     } as ReturnType<typeof usePoolStatic>)
-    vi.mocked(usePoolDynamicContractData).mockReturnValueOnce({
-      entryFee: '0',
-    } as ReturnType<typeof usePoolDynamicContractData>)
 
-    const { result, rerender } = renderHook(() => useIsCustomCooldownDeposit())
+    const { result } = renderHook(() => useIsCustomCooldownDeposit())
 
-    expect(result.current).toBe(false)
-
-    vi.mocked(usePoolStatic).mockReturnValueOnce({
-      data: { isCustomCooldownDepositAllowed: true },
-    } as ReturnType<typeof usePoolStatic>)
-    vi.mocked(usePoolDynamicContractData).mockReturnValueOnce({
-      entryFee: '0',
-    } as ReturnType<typeof usePoolDynamicContractData>)
-
-    act(() => {
-      rerender()
-    })
-    expect(result.current).toBe(false)
-
-    vi.mocked(usePoolStatic).mockReturnValueOnce({
-      data: { isCustomCooldownDepositAllowed: false },
-    } as ReturnType<typeof usePoolStatic>)
-    vi.mocked(usePoolDynamicContractData).mockReturnValueOnce({
-      entryFee: '100',
-    } as ReturnType<typeof usePoolDynamicContractData>)
-
-    act(() => {
-      rerender()
-    })
     expect(result.current).toBe(false)
   })
 
-  it('should return true when entry fee is greater than 0 and custom cooldown deposit is allowed', () => {
+  it('should return true when custom cooldown deposit is allowed', () => {
     vi.mocked(usePoolStatic).mockReturnValueOnce({
       data: { isCustomCooldownDepositAllowed: true },
     } as ReturnType<typeof usePoolStatic>)
-    vi.mocked(usePoolDynamicContractData).mockReturnValueOnce({
-      entryFee: '100',
-    } as ReturnType<typeof usePoolDynamicContractData>)
 
     const { result } = renderHook(() => useIsCustomCooldownDeposit())
 
