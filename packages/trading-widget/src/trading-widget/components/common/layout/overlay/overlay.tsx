123b9dc10c33a5974a034712918308f875050812
Dmitry Lobachevsky <dmitriylobachevskiy@gmail.com>
2024-11-05 16:15:58 +0100

    fix: close overlay on Escape key press
    (cherry picked from commit e8df4469e2947a76b66f8e99b7c777563ecdd4fa)

--- packages/trading-widget/src/trading-widget/components/common/layout/overlay/overlay.tsx
+++ packages/trading-widget/src/trading-widget/components/common/layout/overlay/overlay.tsx
@@ -1,6 +1,7 @@
 import { XMarkIcon } from '@heroicons/react/16/solid'
 import classNames from 'classnames'
 import type { FC, PropsWithChildren } from 'react'
+import { useEffect } from 'react'
 
 import { IconButton } from 'trading-widget/components/common/button/icon-button/icon-button'
 
@@ -17,25 +18,42 @@ export const Overlay: FC<PropsWithChildren<OverlayProps>> = ({
   transparent = false,
   centered = true,
   onClose,
-}) => (
-  <div className="dtw-absolute dtw-top-0 dtw-inset-0 dtw-rounded-xl dtw-backdrop-blur-md dtw-backdrop-brightness-75 dtw-backdrop-filter dtw-p-4">
-    <div
-      className={classNames(
-        'dtw-flex dtw-flex-col dtw-rounded-xl dtw-p-4 dtw-h-full',
-        className,
-        {
-          'dtw-bg-[var(--panel-popup-bg,var(--panel-secondary-color))]':
-            !transparent,
-          'dtw-items-center dtw-justify-center': centered,
-        },
-      )}
-    >
-      {children}
-      {!!onClose && (
-        <div className="dtw-absolute dtw-top-5 dtw-right-5">
-          <IconButton Icon={XMarkIcon} onClick={onClose} />
-        </div>
-      )}
+}) => {
+
+  useEffect(() => {
+    const handleEscKey = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose?.()
+      }
+    }
+
+    window.addEventListener('keydown', handleEscKey)
+
+    return () => {
+      window.removeEventListener('keydown', handleEscKey)
+    }
+  }, [onClose])
+
+  return (
+    <div className="dtw-absolute dtw-top-0 dtw-inset-0 dtw-rounded-xl dtw-backdrop-blur-md dtw-backdrop-brightness-75 dtw-backdrop-filter dtw-p-4">
+      <div
+        className={classNames(
+          'dtw-flex dtw-flex-col dtw-rounded-xl dtw-p-4 dtw-h-full',
+          className,
+          {
+            'dtw-bg-[var(--panel-popup-bg,var(--panel-secondary-color))]':
+              !transparent,
+            'dtw-items-center dtw-justify-center': centered,
+          },
+        )}
+      >
+        {children}
+        {!!onClose && (
+          <div className="dtw-absolute dtw-top-5 dtw-right-5">
+            <IconButton Icon={XMarkIcon} onClick={onClose} />
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-)
+  )
+}
