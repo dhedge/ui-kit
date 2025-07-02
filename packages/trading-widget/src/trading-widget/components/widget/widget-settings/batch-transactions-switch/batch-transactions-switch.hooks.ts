3c6b30336f3ac74ced7edfa428a21e1f842ea42a
Dmitry Lobachevsky <dmitriylobachevskiy@gmail.com>
2025-07-02 13:55:26 +0200

    feat: add logging for batch transactions setting changes

--- packages/trading-widget/src/trading-widget/components/widget/widget-settings/batch-transactions-switch/batch-transactions-switch.hooks.ts
+++ packages/trading-widget/src/trading-widget/components/widget/widget-settings/batch-transactions-switch/batch-transactions-switch.hooks.ts
@@ -1,6 +1,13 @@
 import { useCallback, useMemo } from 'react'
 
-import { useTradingPanelSettings } from 'core-kit/hooks/state'
+import {
+  TRADING_LOG_EVENT_PARAM,
+  TRADING_PANEL_LOG_EVENT,
+} from 'core-kit/const'
+import {
+  useTradingPanelLogger,
+  useTradingPanelSettings,
+} from 'core-kit/hooks/state'
 
 export const useBatchTransactionsSwitch = (): [
   boolean,
@@ -9,11 +16,18 @@ export const useBatchTransactionsSwitch = (): [
   const [{ isBatchTransactionsEnabled }, updateSettings] =
     useTradingPanelSettings()
 
+  const log = useTradingPanelLogger()
+
   const setIsBatchTransactions = useCallback(
     (enabled: boolean) => {
       updateSettings({ isBatchTransactionsEnabled: enabled })
+      log?.(TRADING_PANEL_LOG_EVENT.BATCH_TRANSACTIONS_CHANGE, {
+        [TRADING_LOG_EVENT_PARAM.IS_BATCH_TRANSACTIONS_ENABLED.NAME]: enabled
+          ? 1
+          : 0,
+      })
     },
-    [updateSettings],
+    [updateSettings, log],
   )
 
   return useMemo(
