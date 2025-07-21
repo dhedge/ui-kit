decd905c8f8c38f5af3c1f293f252d60e18579e9
Denis Ignatovich <denis.ignatovich.m@gmail.com>
2025-07-21 19:20:42 +0200

    fix(useIsUnrollAndClaimTransaction): Modify conditions to disable within vault

--- src/core-kit/hooks/trading/withdraw-v2/init-step/use-is-unroll-and-claim-transaction.ts
+++ src/core-kit/hooks/trading/withdraw-v2/init-step/use-is-unroll-and-claim-transaction.ts
@@ -1,13 +1,16 @@
 import { useReceiveTokenInput } from 'core-kit/hooks/state'
 import { useInitWithdrawEstimatedReceiveAssets } from 'core-kit/hooks/trading/withdraw-v2/init-step/use-init-withdraw-estimated-receive-assets'
+import { useIsDhedgeVaultConnected } from 'core-kit/hooks/user'
 import { isEqualAddress } from 'core-kit/utils'
 
 export const useIsUnrollAndClaimTransaction = () => {
   const [receiveToken] = useReceiveTokenInput()
   const { data: assets = [] } = useInitWithdrawEstimatedReceiveAssets()
+  const isDhedgeVaultConnected = useIsDhedgeVaultConnected()
 
   return (
     assets?.length === 1 &&
-    isEqualAddress(assets[0]?.address, receiveToken.address)
+    isEqualAddress(assets[0]?.address, receiveToken.address) &&
+    !isDhedgeVaultConnected // EasySwapperV2::unrollAndClaim is not supported from within dHEDGE vaults. Managers should be able to use regular initWithdrawal & completeWithdrawal flow instead.
   )
 }
