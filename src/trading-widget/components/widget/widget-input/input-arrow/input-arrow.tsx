5f11e3668c20face38ac9c6979e36536e7feadaf
Yavorski <artsiom.yavorski@scorum.net>
2025-08-28 23:47:12 +0300

    refactor: update input arrow button

--- src/trading-widget/components/widget/widget-input/input-arrow/input-arrow.tsx
+++ src/trading-widget/components/widget/widget-input/input-arrow/input-arrow.tsx
@@ -8,7 +8,7 @@ export const InputArrow: FC = () => {
   return (
     <div
       onClick={handleClick}
-      className="dtw-cursor-pointer dtw-group dtw-absolute dtw-left-1/2 -dtw-top-[calc(var(--panel-inputs-group-gap)*0.5)] -dtw-translate-y-1/2 -dtw-translate-x-1/2 dtw-flex dtw-items-center dtw-justify-center dtw-rounded-[var(--panel-inputs-arrow-radius)] dtw-border-2 dtw-border-[color:var(--panel-background-color)]"
+      className="dtw-cursor-pointer dtw-group dtw-absolute dtw-left-1/2 -dtw-top-[calc(var(--panel-inputs-group-gap)*0.5)] -dtw-translate-y-1/2 -dtw-translate-x-1/2 dtw-flex dtw-items-center dtw-justify-center dtw-rounded-[var(--panel-inputs-arrow-radius)] dtw-p-0.5 dtw-bg-[color:var(--panel-background-color)] dtw-backdrop-blur-3xl"
     >
       <button className="!dtw-bg-[var(--panel-inputs-arrow-bg)] dtw-rounded-[var(--panel-inputs-arrow-radius)] dtw-px-[var(--panel-inputs-group-px-arrow)] dtw-py-[var(--panel-inputs-group-py-arrow)]">
         <ArrowDownIcon className="dtw-h-[var(--panel-inputs-group-arrow-size)] dtw-w-[var(--panel-inputs-group-arrow-size)] dtw-text-[color:var(--panel-inputs-arrow-color)] group-hover:dtw-opacity-[var(--panel-action-hover-opacity)] group-hover:dtw-rotate-180 dtw-transition-transform" />
