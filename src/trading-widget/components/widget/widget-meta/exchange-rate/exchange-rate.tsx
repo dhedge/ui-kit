975b3278cb1db4f20838e8080547f8b60d567958
Yavorski <artsiom.yavorski@scorum.net>
2025-08-29 22:19:10 +0300

    feat: update exchange rate style

--- src/trading-widget/components/widget/widget-meta/exchange-rate/exchange-rate.tsx
+++ src/trading-widget/components/widget/widget-meta/exchange-rate/exchange-rate.tsx
@@ -15,16 +15,16 @@ export const ExchangeRate: FC<ExchangeRateProps> = ({ className }) => {
   return (
     <div
       className={classNames(
-        'dtw-flex dtw-h-[var(--panel-icon-secondary-size)] sm:dtw-h-[var(--panel-icon-secondary-size-sm)] dtw-justify-end dtw-gap-x-2',
+        'dtw-flex dtw-h-[var(--panel-icon-secondary-size)] dtw-justify-end dtw-gap-x-2',
         className,
       )}
     >
       {isLoading && (
-        <Spinner className="dtw-stroke-[color:var(--panel-accent-from-color)] dtw-h-[var(--panel-icon-secondary-size)] sm:dtw-h-[var(--panel-icon-secondary-size-sm)] dtw-w-[var(--panel-icon-secondary-size)] sm:dtw-w-[var(--panel-icon-secondary-size-sm)]" />
+        <Spinner className="dtw-stroke-[color:var(--panel-accent-from-color)] dtw-h-[var(--panel-icon-secondary-size)] dtw-w-[var(--panel-icon-secondary-size)]" />
       )}
       <p
         className={classNames(
-          'dtw-text-right dtw-text-[length:var(--panel-label-font-size)] dtw-leading-[var(--panel-label-line-height,var(--panel-line-height-xs))] dtw-text-[color:var(--panel-secondary-content-color)] dtw-font-[var(--panel-meta-font-weight,var(--panel-font-weight-light))]',
+          'dtw-text-right dtw-text-[length:var(--panel-meta-font-size)] dtw-leading-[var(--panel-meta-line-height)] dtw-text-[color:var(--panel-secondary-content-color)] dtw-font-[var(--panel-meta-font-weight,var(--panel-font-weight-light))]',
           {
             'dtw-text-[color:var(--panel-loading-content-color)]': isLoading,
           },
